42f4fe943af785fe324f8baf4284cab63494cc9f
MrEggoXYZ <84375166+MrEggoXYZ@users.noreply.github.com>
2021-09-03 17:16:12 +1000

    added base files

---
+++ Node Snips/main.js
@@ -0,0 +1,16 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sleep(ms) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
+}
+
+
+
+module.exports = {
+    getRandomInt,
+    sleep
+}
